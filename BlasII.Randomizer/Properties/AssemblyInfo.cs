2a123f9a13943ff0dccc30ea18a75a5b17728251
Branden Wood <brandenek03912@hotmail.com>
2023-09-12 22:12:29 -0400

    Add dependency on api

--- BlasII.Randomizer/Properties/AssemblyInfo.cs
+++ BlasII.Randomizer/Properties/AssemblyInfo.cs
@@ -2,4 +2,4 @@ using BlasII.Randomizer;
 using MelonLoader;
 
 [assembly: MelonInfo(typeof(BlasII.Randomizer.Main), ModInfo.MOD_NAME, ModInfo.MOD_VERSION, ModInfo.MOD_AUTHOR)]
-[assembly: MelonAdditionalDependencies("ModdingAPI")]
+[assembly: MelonOptionalDependencies("Modding API", "0.2.0")]
