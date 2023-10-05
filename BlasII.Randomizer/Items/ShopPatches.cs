4f3effec233f23b646021ff7cd1445a17f5ded10
Branden Wood <brandenek03912@hotmail.com>
2023-10-05 18:38:03 -0400

    shops: correctly set item image

--- BlasII.Randomizer/Items/ShopPatches.cs
+++ BlasII.Randomizer/Items/ShopPatches.cs
@@ -6,7 +6,9 @@ using Il2CppTGK.Game.Managers;
 using Il2CppTGK.Game.ShopSystem;
 using Il2CppTGK.Inventory;
 using Il2CppTGK.UI;
+using UnityEngine;
 using UnityEngine.UI;
+using static MelonLoader.MelonLogger;
 
 namespace BlasII.Randomizer.Items
 {
@@ -58,83 +60,35 @@ namespace BlasII.Randomizer.Items
     }
 
     /// <summary>
-    /// When opening the shop or purchasing an item, update the icons for all items in the shop - not anymore
-    /// </summary>
-    [HarmonyPatch(typeof(ShopWindowLogic), nameof(ShopWindowLogic.UpdateTabs))]
-    class Shop_Update_Patch
-    {
-        public static void Postfix(ShopWindowLogic __instance)
-        {
-            Main.Randomizer.LogWarning("Updating tabs!");
-        }
-    }
-
-    /// <summary>
-    /// When selecting a different item, update the name and description text
-    /// </summary>
-    [HarmonyPatch(typeof(ShopWindowLogic), nameof(ShopWindowLogic.OnSelectIem))]
-    class Shop_Select_Patch
-    {
-        public static void Postfix(ShopWindowLogic __instance)
-        {
-            //Main.Randomizer.LogWarning("Selecting new item!");
-            //var lance = Main.Randomizer.Data.GetItem("QI70"); // Get random item
-
-            //__instance.captionText.SetText(lance.Upgraded.name);
-            //__instance.descriptionText.textControl.SetText(lance.Upgraded.Description);
-        }
-    }
-
-    /// <summary>
-    /// When opening the confirmation box, change the item name displayed
-    /// </summary>
-    [HarmonyPatch(typeof(ShopWindowLogic), nameof(ShopWindowLogic.ShowConfirmation))]
-    class Shop_Confirm_Patch
-    {
-        public static void Postfix(ShopWindowLogic __instance)
-        {
-            //Main.Randomizer.LogWarning("Opening confirm box!");
-            //var lance = Main.Randomizer.Data.GetItem("QI70"); // Get random item
-
-            //string originalMessage = __instance.confirmationText.normalText.text;
-            //string firstPart = originalMessage[..(originalMessage.IndexOf('>') + 1)];
-            //string lastPart = originalMessage[originalMessage.LastIndexOf('<')..];
-            //string newMessage = firstPart + lance.Upgraded.name + lastPart;
-
-            //__instance.confirmationText.SetText(newMessage);
-        }
-    }
-
-    /// <summary>
-    /// When opening the shop or purchasing an item, update the icons for all items in the shop 
-    /// </summary>
-    [HarmonyPatch(typeof(ShopWindowLogic), nameof(ShopWindowLogic.OnCreateListItem))]
-    class Shop_Move_Patch
-    {
-        public static void Postfix(UINavigableScrollableList.ScrollableListData data)
-        {
-            Main.Randomizer.LogWarning("Creating new scroll item!");
-            var lance = Main.Randomizer.Data.GetItem("QI70"); // Get random item
-
-            data.obj.transform.Find("Image").GetComponent<Image>().sprite = lance.Upgraded.Image;
-
-            Main.Randomizer.Log(data.obj.name);
-        }
-    }
-
-    /// <summary>
-    /// Change the name and description for this shop item to its random item
+    /// When opening the shop or purchasing an item, update this item's name and description
     /// </summary>
     [HarmonyPatch(typeof(ShopListItem), nameof(ShopListItem.SetData))]
-    class Shop_Data_Patch
+    class Shop_Text_Patch
     {
         public static void Postfix(ShopListItem __instance)
         {
-            Main.Randomizer.LogWarning("Updating list item data!");
+            string locationId = $"{Object.FindObjectOfType<ShopWindowLogic>().currentShop.name}.o{__instance.OrbIdx}";
+            Main.Randomizer.LogWarning("Setting data for " + locationId);
             var lance = Main.Randomizer.Data.GetItem("QI70"); // Get random item
 
             __instance.Caption = lance.Upgraded.name;
             __instance.Description = lance.Description;
+        }
+    }
+
+    /// <summary>
+    /// When opening the shop or purchasing an item, update this item's image
+    /// </summary>
+    [HarmonyPatch(typeof(ShopWindowLogic), nameof(ShopWindowLogic.OnCreateListItem))]
+    class Shop_Image_Patch
+    {
+        public static void Postfix(UINavigableScrollableList.ScrollableListData data)
+        {
+            string locationId = $"{Object.FindObjectOfType<ShopWindowLogic>().currentShop.name}.o{data.obj.GetComponent<ShopListItem>().OrbIdx}";
+            Main.Randomizer.LogWarning("Setting image for " + locationId);
+            var lance = Main.Randomizer.Data.GetItem("QI70"); // Get random item
+
+            data.obj.transform.Find("Image").GetComponent<Image>().sprite = lance.Upgraded.Image;
         }
     }
 }
