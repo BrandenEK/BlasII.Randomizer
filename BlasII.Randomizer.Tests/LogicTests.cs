be1818a77d3199dd34dc5eac75fd53c4574aaf10
Branden <44546325+BrandenEK@users.noreply.github.com>
2023-10-19 11:12:00 -0400

    tests: load all json data on start

--- BlasII.Randomizer.Tests/LogicTests.cs
+++ BlasII.Randomizer.Tests/LogicTests.cs
@@ -1,17 +1,43 @@
+using BlasII.Randomizer.Doors;
 using BlasII.Randomizer.Items;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.IO;
 
 namespace BlasII.Randomizer.Tests
 {
     [TestClass]
     public class LogicTests
     {
+        private static readonly Dictionary<string, Item> _allItems = new();
+        private static readonly Dictionary<string, ItemLocation> _allItemLocations = new();
+        private static readonly Dictionary<string, DoorLocation> _allDoors = new();
+
+        [ClassInitialize]
+        public static void LoadJsonData(TestContext context)
+        {
+            string dataFolder = "../../../../resources/data/Randomizer/";
+
+            string items = File.ReadAllText(dataFolder + "items.json");
+            foreach (var item in JsonConvert.DeserializeObject<Item[]>(items))
+                _allItems.Add(item.id, item);
+
+            string itemLocations = File.ReadAllText(dataFolder + "item-locations.json");
+            foreach (var itemLocation in JsonConvert.DeserializeObject<ItemLocation[]>(itemLocations))
+                _allItemLocations.Add(itemLocation.id, itemLocation);
+
+            string doors = File.ReadAllText(dataFolder + "doors.json");
+            foreach (var door in JsonConvert.DeserializeObject<DoorLocation[]>(doors))
+                _allDoors.Add(door.id, door);
+        }
+
         [TestMethod]
         public void TestMethod1()
         {
             var inventory = new Blas2Inventory();
 
-            Assert.IsNotNull(inventory);
+            Assert.AreEqual(176, _allItems.Count);
         }
     }
 }
