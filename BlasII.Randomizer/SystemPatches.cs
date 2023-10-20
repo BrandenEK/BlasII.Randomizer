3a3a515093a4c5c960acb1f72e5d0542c48f3d68
Branden Wood <brandenek03912@hotmail.com>
2023-10-20 11:01:25 -0400

    gameplay: prevent camera lock in boss rooms

--- BlasII.Randomizer/SystemPatches.cs
+++ BlasII.Randomizer/SystemPatches.cs
@@ -62,4 +62,35 @@ namespace BlasII.Randomizer
             __result = __result || itemID.name == "QI70";
         }
     }
+
+    /// <summary>
+    /// When reloading a boss room after the fight, force deactivate it to prevent camera lock
+    /// </summary>
+    [HarmonyPatch(typeof(RoomManager), nameof(RoomManager.ChangeRoom))]
+    class Room_Change_Patch
+    {
+        public static void Prefix(int roomHash, ref bool forceDeactivate)
+        {
+            foreach (int room in bossRooms)
+            {
+                if (roomHash == room)
+                {
+                    Main.Randomizer.Log("Force deactivating boss room");
+                    forceDeactivate = true;
+                }
+            }
+        }
+
+        private static readonly int[] bossRooms =
+        {
+            129108797,
+            -1436975306,
+            129108570,
+            // 1574233179, Causes missing fog vfx
+            // Benedicta changes rooms
+            -133013164,
+            1433070649,
+            1433070681,
+        };
+    }
 }
