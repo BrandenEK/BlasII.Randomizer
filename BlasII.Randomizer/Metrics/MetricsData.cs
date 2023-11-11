5b30f916a2b41d77adebe19fbd3a33766862be15
Branden Wood <brandenek03912@hotmail.com>
2023-11-11 14:59:22 -0500

    metrics: send data to api

---
+++ BlasII.Randomizer/Metrics/MetricsData.cs
@@ -0,0 +1,15 @@
+﻿
+namespace BlasII.Randomizer.Metrics
+{
+    internal class MetricsData
+    {
+        public int sw;
+        public int rq;
+
+        public MetricsData(RandomizerSettings settings)
+        {
+            sw = settings.startingWeapon;
+            rq = settings.requiredKeys;
+        }
+    }
+}
--- BlasII.Randomizer/Metrics/MetricsHandler.cs
+++ BlasII.Randomizer/Metrics/MetricsHandler.cs
@@ -1,6 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using Newtonsoft.Json;
+using System;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -13,7 +13,30 @@ namespace BlasII.Randomizer.Metrics
             if (!Main.Randomizer.ConfigHandler.GetProperty<bool>("SendMetrics"))
                 return;
 
-            Main.Randomizer.Log("Sending metrics!");
+            string url = $"http://blas2.randomizer.mooo.com/api/settings";
+            string json = JsonConvert.SerializeObject(new MetricsData(settings));
+
+            Main.Randomizer.Log($"Attempting to send metrics data...");
+
+            try
+            {
+                var client = new HttpClient();
+                var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+                var response = await client.PostAsync(url, content);
+
+                if (!response.IsSuccessStatusCode)
+                {
+                    string responseContent = await response.Content.ReadAsStringAsync();
+                    throw new Exception(responseContent);
+                }
+
+                Main.Randomizer.Log("Successfully sent metrics data");
+            }
+            catch (Exception e)
+            {
+                Main.Randomizer.LogError("Failed to send metrics data: " + e.Message);
+            }
         }
     }
 }
