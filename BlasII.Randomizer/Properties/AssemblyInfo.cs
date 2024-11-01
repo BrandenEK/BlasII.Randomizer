53702041fbcbfbe9df2defd6712512b8deee76f8
Branden Wood <44546325+BrandenEK@users.noreply.github.com>
2024-10-31 21:16:08 -0400

    core: remove UI Framework dependency

--- BlasII.Randomizer/Properties/AssemblyInfo.cs
+++ BlasII.Randomizer/Properties/AssemblyInfo.cs
@@ -2,4 +2,4 @@ using BlasII.Randomizer;
 using MelonLoader;
 
 [assembly: MelonInfo(typeof(BlasII.Randomizer.Main), ModInfo.MOD_NAME, ModInfo.MOD_VERSION, ModInfo.MOD_AUTHOR)]
-[assembly: MelonOptionalDependencies("Modding API", "1.0.2", "Menu Framework", "0.1.0", "UI Framework", "0.1.0", "Quality of Life", "1.3.0")]
+[assembly: MelonOptionalDependencies("Modding API", "1.0.2", "Menu Framework", "0.1.0", "Quality of Life", "1.3.0")]
