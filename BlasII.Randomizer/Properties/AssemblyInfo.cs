bb339a9662d61ebb1d057c103aa689bc9dd02ff0
Branden Wood <44546325+BrandenEK@users.noreply.github.com>
2025-03-08 13:11:39 -0500

    core: upgrade API version

--- BlasII.Randomizer/Properties/AssemblyInfo.cs
+++ BlasII.Randomizer/Properties/AssemblyInfo.cs
@@ -2,4 +2,4 @@ using BlasII.Randomizer;
 using MelonLoader;
 
 [assembly: MelonInfo(typeof(BlasII.Randomizer.Main), ModInfo.MOD_NAME, ModInfo.MOD_VERSION, ModInfo.MOD_AUTHOR)]
-[assembly: MelonOptionalDependencies("Modding API", "1.1.1", "Menu Framework", "0.2.0")]
+[assembly: MelonOptionalDependencies("Modding API", "2.0.0", "Menu Framework", "0.2.0")]
