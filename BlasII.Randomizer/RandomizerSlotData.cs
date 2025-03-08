9965b8be9023b9a25d726ba38b893a9e1e75a123
Branden Wood <44546325+BrandenEK@users.noreply.github.com>
2025-03-08 13:21:39 -0500

    core: update save system with new API (#182)
    * core: upgrade API version
    
    * core: redo the persistent mod declaration

---
+++ BlasII.Randomizer/RandomizerSlotData.cs
@@ -0,0 +1,30 @@
+﻿using BlasII.ModdingAPI.Persistence;
+using System.Collections.Generic;
+
+namespace BlasII.Randomizer;
+
+/// <summary>
+/// Slot save data for the Randomizer
+/// </summary>
+public class RandomizerSlotData : SlotSaveData
+{
+    /// <summary>
+    /// The location to item mapping
+    /// </summary>
+    public Dictionary<string, string> mappedItems;
+
+    /// <summary>
+    /// The list of collected locations
+    /// </summary>
+    public List<string> collectedLocations;
+
+    /// <summary>
+    /// The list of collected items
+    /// </summary>
+    public List<string> collectedItems;
+
+    /// <summary>
+    /// The selected settings
+    /// </summary>
+    public RandomizerSettings settings;
+}
