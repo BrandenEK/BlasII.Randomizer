5b2c3d45589a21627dfc08ef7ad2de17a14b992f
Branden Wood <44546325+BrandenEK@users.noreply.github.com>
2025-03-14 08:13:12 -0400

    bench: move more code to runner

--- BlasII.Randomizer.Benchmarks/BenchmarkRunner.cs
+++ BlasII.Randomizer.Benchmarks/BenchmarkRunner.cs
@@ -1,10 +1,21 @@
 ﻿using BlasII.Randomizer.Benchmarks.Exporters;
+using BlasII.Randomizer.Benchmarks.Metrics;
+using System.Diagnostics;
+using System.Reflection;
+using System.Text;
 
 namespace BlasII.Randomizer.Benchmarks;
 
 public class BenchmarkRunner
 {
     private static readonly List<IExporter> _exporters = new();
+
+    public void Run(int iterations)
+    {
+        // Do the actual stuff
+    }
+
+    // Adding exporters
 
     public BenchmarkRunner AddExporter(IExporter exporter)
     {
@@ -32,8 +43,76 @@ public class BenchmarkRunner
         return this;
     }
 
-    public void Run(int iterations)
+    // Calculating display
+
+    private static string GetInfoDisplay(int iterationCount)
     {
-        // Do the actual stuff
+        var text = new List<string>()
+        {
+            $" Machine: {Environment.MachineName} {(Environment.Is64BitOperatingSystem ? "x64" : "x86")} ({Environment.ProcessorCount} processors)",
+            $" Operating system: {Environment.OSVersion}",
+            $" Start time: {DateTime.Now:MM/dd/yy H:mm:ss}",
+            $" Max iterations: {iterationCount}",
+            $" Debug mode: {Assembly.GetExecutingAssembly().GetCustomAttributes(false).OfType<DebuggableAttribute>().Any(x => x.IsJITTrackingEnabled)}",
+        };
+
+        var line = new string('=', text.Max(x => x.Length) + 1);
+
+        var sb = new StringBuilder();
+        sb.AppendLine();
+        sb.AppendLine(line);
+        sb.AppendJoin(Environment.NewLine, text);
+        sb.AppendLine();
+        sb.AppendLine(line);
+        sb.AppendLine();
+
+        return sb.ToString();
+    }
+
+    private static string GetResultsDisplay(string[,] results)
+    {
+        var sb = new StringBuilder();
+
+        // Fill header row
+        results[0, 0] = "Method";
+        results[0, 1] = "Parameters";
+        for (int i = 0; i < _metrics.Length; i++)
+            results[0, i + 2] = _metrics[i].DisplayName;
+
+        // Calculate maxwidth for each column
+        var columnWidths = new int[results.GetLength(1)];
+        for (int y = 0; y < results.GetLength(1); y++)
+        {
+            for (int x = 0; x < results.GetLength(0); x++)
+            {
+                if (results[x, y].Length > columnWidths[y])
+                    columnWidths[y] = results[x, y].Length;
+            }
+        }
+
+        // Add formatted data to the outout
+        for (int x = 0; x < results.GetLength(0); x++)
+        {
+            char padding = x == 0 ? '-' : ' ';
+
+            sb.Append('|');
+            for (int y = 0; y < results.GetLength(1); y++)
+            {
+                sb.Append($" {results[x, y].PadLeft(columnWidths[y], ' ')} |");
+            }
+            sb.AppendLine();
+
+            if (x >= results.GetLength(0) - 1 || results[x, 0] == results[x + 1, 0])
+                continue;
+
+            sb.Append('|');
+            for (int y = 0; y < results.GetLength(1); y++)
+            {
+                sb.Append($"{new string(padding, columnWidths[y] + 2)}|");
+            }
+            sb.AppendLine();
+        }
+
+        return sb.ToString();
     }
 }
