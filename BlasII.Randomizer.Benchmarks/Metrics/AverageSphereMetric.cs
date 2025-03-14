a0e0b424be55badafe2ef6b99683371f6ddc764d
Branden Wood <44546325+BrandenEK@users.noreply.github.com>
2025-03-14 11:14:11 -0400

    benchmarks: add sphere count metric (#190)
    * bench: add sphere metric
    
    * bench: move data storage to core
    
    * bench: implement sphere count metric
    
    * bench: finalize sphere metric
    
    * bench: use flat 200 warmups

---
+++ BlasII.Randomizer.Benchmarks/Metrics/AverageSphereMetric.cs
@@ -0,0 +1,73 @@
+﻿using BlasII.ModdingAPI.Assets;
+using BlasII.Randomizer.Benchmarks.Models;
+using BlasII.Randomizer.Models;
+using BlasII.Randomizer.Shuffle;
+
+namespace BlasII.Randomizer.Benchmarks.Metrics;
+
+public class AverageSphereMetric : IMetric<BenchmarkResult>
+{
+    public string DisplayName { get; } = "Avg. Sphere Count";
+
+    private int successfulSpheres;
+    private int successfulAttempts;
+
+    public void HandleResult(BenchmarkResult result, TimeSpan time)
+    {
+        if (!result.WasSuccessful)
+            return;
+
+        successfulSpheres += CalculateSphereCount(result.Mapping, result.Settings);
+        successfulAttempts++;
+    }
+
+    public string FormatMetric()
+    {
+        if (successfulAttempts == 0)
+            throw new DivideByZeroException("Attempts was zero");
+
+        double value = (double)successfulSpheres / successfulAttempts;
+        return $"{value:F1}";
+    }
+
+    public void Reset()
+    {
+        successfulSpheres = 0;
+        successfulAttempts = 0;
+    }
+
+    private int CalculateSphereCount(Dictionary<string, string> mapping, RandomizerSettings settings)
+    {
+        int sphere = 0;
+        var remainingLocations = new List<ItemLocation>(Core.ItemLocations.Values);
+        var reachedLocations = new List<ItemLocation>();
+        var inventory = BlasphemousInventory.CreateNewInventory(settings);
+        inventory.Add(((WEAPON_IDS)settings.RealStartingWeapon).ToString());
+
+        while (remainingLocations.Count > 0)
+        {
+            foreach (var location in remainingLocations)
+            {
+                if (inventory.Evaluate(location.Logic))
+                    reachedLocations.Add(location);
+            }
+
+            foreach (var location in reachedLocations)
+            {
+                remainingLocations.Remove(location);
+
+                Item item = Core.Items[mapping[location.Id]];
+                if (item.Progression)
+                    inventory.Add(item.Id);
+            }
+
+            reachedLocations.Clear();
+            sphere++;
+
+            if (sphere >= Core.ItemLocations.Count)
+                throw new Exception($"Not all locations are reachable in sphere calculation");
+        }
+
+        return sphere;
+    }
+}
