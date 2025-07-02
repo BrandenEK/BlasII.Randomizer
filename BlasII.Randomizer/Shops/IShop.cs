297bc2425bdc94bdf1f46c9e8a23f50b2f6ed38e
Branden Wood <44546325+BrandenEK@users.noreply.github.com>
2025-07-02 15:50:09 -0400

    shops: add shop data

---
+++ BlasII.Randomizer/Shops/IShop.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+
+namespace BlasII.Randomizer.Shops;
+
+/// <summary>
+/// Provides functionality for one of the shops
+/// </summary>
+public interface IShop
+{
+    /// <summary>
+    /// Returns a list of the item costs in the shop
+    /// </summary>
+    public IEnumerable<int> GetVanillaCosts();
+}
+
+internal class HandShop : IShop
+{
+    private readonly int[] _costs = [3000, 3000, 3000, 3000, 3000, 3000, 3000, 6000, 12000, 12000, 17500, 32000];
+
+    public IEnumerable<int> GetVanillaCosts()
+    {
+        return _costs;
+    }
+}
+
+internal class PatioShop : IShop
+{
+    private readonly int[] _costs = [6000, 6000, 6000, 6000, 12000, 12000, 12000];
+
+    public IEnumerable<int> GetVanillaCosts()
+    {
+        return _costs;
+    }
+}
+
+internal class TravelerShop : IShop
+{
+    public IEnumerable<int> GetVanillaCosts()
+    {
+        yield return 3000;
+        yield return 3000;
+        if (TEMP_VALUE >= 0) yield return 6000;
+        if (TEMP_VALUE >= 1) yield return 6000;
+        if (TEMP_VALUE >= 2) yield return 6000;
+        if (TEMP_VALUE >= 3) yield return 6000;
+        if (TEMP_VALUE >= 4) yield return 12000;
+        if (TEMP_VALUE >= 5) yield return 17500;
+    }
+
+    private const int TEMP_VALUE = 3;
+}
