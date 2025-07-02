75eba04034db9ad1ce69cd0b1cf0a41d7def69d6
Branden Wood <44546325+BrandenEK@users.noreply.github.com>
2025-07-02 16:30:26 -0400

    shops: move patches to proper namespace

--- BlasII.Randomizer/Shops/ShopPatches.cs
+++ BlasII.Randomizer/Shops/ShopPatches.cs
@@ -11,7 +11,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-namespace BlasII.Randomizer.Patches;
+namespace BlasII.Randomizer.Shops;
 
 /// <summary>
 /// Setup the randomized items for a shop
