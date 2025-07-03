e43916fac306878402ac258b0130e701eb9ccd8f
Branden Wood <44546325+BrandenEK@users.noreply.github.com>
2025-07-02 20:16:38 -0400

    shops: add ShopValue enum

---
+++ BlasII.Randomizer/Shops/ShopValue.cs
@@ -0,0 +1,25 @@
+﻿
+namespace BlasII.Randomizer.Shops;
+
+/// <summary>
+/// Characterizes how an item will be priced in the shop
+/// </summary>
+public enum ShopValue
+{
+    /// <summary> Determines shop price </summary>
+    FillerInventory,
+    /// <summary> Determines shop price </summary>
+    UsefulInventory,
+    /// <summary> Determines shop price </summary>
+    ProgressionInventory,
+    /// <summary> Determines shop price </summary>
+    WeaponsAndAbilities,
+    /// <summary> Determines shop price </summary>
+    Cherubs,
+    /// <summary> Determines shop price </summary>
+    BossKeys,
+    /// <summary> Determines shop price </summary>
+    LowTears,
+    /// <summary> Determines shop price </summary>
+    HighTears,
+}
