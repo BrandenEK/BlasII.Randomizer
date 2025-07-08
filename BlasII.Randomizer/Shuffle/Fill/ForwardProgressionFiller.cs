1983691374f3ae836ff2b615b3b41592b84f97e9
Branden Wood <44546325+BrandenEK@users.noreply.github.com>
2025-07-07 20:31:12 -0400

    shuffle: break filler

---
+++ BlasII.Randomizer/Shuffle/Fill/ForwardProgressionFiller.cs
@@ -0,0 +1,55 @@
+﻿using Basalt.LogicParser;
+using BlasII.Randomizer.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BlasII.Randomizer.Shuffle.Fill;
+
+internal class ForwardProgressionFiller : IFiller
+{
+    private readonly Dictionary<string, Item> _items;
+
+    public ForwardProgressionFiller(Dictionary<string, Item> items)
+    {
+        _items = items;
+    }
+
+    public void Fill(LocationPool locations, ItemPool items, Dictionary<string, string> output, GameInventory inventory)
+    {
+        items.Shuffle();
+        MovePriorityItems(items);
+        var reachableLocations = FindReachableLocations(locations, inventory);
+
+        while (reachableLocations.Size > 0 && items.Size > 0)
+        {
+            ItemLocation location = reachableLocations.RemoveRandom();
+            locations.Remove(location);
+
+            Item item = items.RemoveLast();
+            inventory.Add(item.Id);
+
+            output.Add(location.Id, item.Id);
+            reachableLocations = FindReachableLocations(locations, inventory);
+        }
+    }
+
+    private void MovePriorityItems(ItemPool progressionItems)
+    {
+        Item wallClimb = _items["WallClimb"];
+        progressionItems.MoveToEnd(wallClimb);
+    }
+
+    private LocationPool FindReachableLocations(LocationPool locations, GameInventory inventory)
+    {
+        var temp = new LocationPool(locations);
+        temp.Clear();
+
+        foreach (var location in locations.Where(x => inventory.Evaluate(x.Logic)))
+        {
+            temp.Add(location);
+        }
+
+        // Not ideal, but thats how the pools are set up
+        return temp;
+    }
+}
--- BlasII.Randomizer/Shuffle/Shufflers/ComponentShuffler.cs
+++ BlasII.Randomizer/Shuffle/Shufflers/ComponentShuffler.cs
@@ -1,5 +1,6 @@
 ﻿using Basalt.LogicParser;
 using BlasII.Randomizer.Models;
+using BlasII.Randomizer.Shuffle.Fill;
 using BlasII.Randomizer.Shuffle.Inventory;
 using BlasII.Randomizer.Shuffle.Pools;
 using System;
@@ -16,6 +17,8 @@ public class ComponentShuffler : IShuffler
     private readonly IItemPoolCreator _itemPoolCreator;
     private readonly IPoolBalancer _poolBalancer;
     private readonly IInventoryCreator _inventoryCreator;
+    private readonly IFiller _progressionFiller;
+    private readonly IFiller _junkFiller;
 
     public ComponentShuffler(Dictionary<string, ItemLocation> locations, Dictionary<string, Item> items, bool useReverseFill)
     {
@@ -26,6 +29,8 @@ public class ComponentShuffler : IShuffler
         _itemPoolCreator = new ItemPoolCreator(items);
         _poolBalancer = new PoolBalancer(items);
         _inventoryCreator = useReverseFill ? new ReverseInventoryCreator() : new ForwardInventoryCreator();
+        _progressionFiller = useReverseFill ? new ReverseProgressionFiller(_allItems) : new ForwardProgressionFiller(_allItems);
+        _junkFiller = new JunkFiller();
     }
 
     public bool Shuffle(int seed, RandomizerSettings settings, Dictionary<string, string> output)
@@ -46,7 +51,7 @@ public class ComponentShuffler : IShuffler
         _inventoryCreator.Create(settings, progItems, out GameInventory inventory);
 
         // Place progression items at progression locations
-        FillProgressionItems(progLocations, progItems, output, inventory);
+        _progressionFiller.Fill(progLocations, progItems, output, inventory);
 
         // Add junk items to remaining progression items and ensure they are still balanced
         junkLocations.Combine(progLocations);
@@ -54,73 +59,9 @@ public class ComponentShuffler : IShuffler
             return false;
 
         // Place junk items at junk locations and remaining progression locations
-        FillJunkItems(junkLocations, junkItems, output);
+        _junkFiller.Fill(junkLocations, junkItems, output, inventory);
 
         // Verify that all remaining items were placed
         return junkItems.Size == 0 && junkLocations.Size == 0;
-    }
-
-    /// <summary>
-    /// Using a reverse fill algorithm, place the last item at a random reachable location
-    /// </summary>
-    private void FillProgressionItems(LocationPool locations, ItemPool items, Dictionary<string, string> output, GameInventory inventory)
-    {
-        // Verify that all locations are reachable
-        foreach (var location in locations)
-        {
-            if (!inventory.Evaluate(location.Logic))
-                return;
-        }
-
-        items.Shuffle();
-        MovePriorityItems(items);
-        var reachableLocations = new LocationPool(locations);
-
-        while (reachableLocations.Size > 0 && items.Size > 0)
-        {
-            Item item = items.RemoveLast();
-            inventory.Remove(item.Id);
-
-            RemoveUnreachableLocations(reachableLocations, inventory);
-            if (reachableLocations.Size == 0)
-                return;
-
-            ItemLocation location = reachableLocations.RemoveRandom();
-            locations.Remove(location);
-            output.Add(location.Id, item.Id);
-        }
-    }
-
-    /// <summary>
-    /// Without logic, place the last item at a random location
-    /// </summary>
-    private void FillJunkItems(LocationPool locations, ItemPool items, Dictionary<string, string> output)
-    {
-        items.Shuffle();
-
-        while (locations.Size > 0 && items.Size > 0)
-        {
-            ItemLocation location = locations.RemoveLast();
-            Item item = items.RemoveLast();
-
-            output.Add(location.Id, item.Id);
-        }
-    }
-
-    /// <summary>
-    /// After shuffling the list of progression items, move certain items to the end to prevent failing seeds
-    /// </summary>        
-    private void MovePriorityItems(ItemPool progressionItems)
-    {
-        Item wallClimb = _allItems["WallClimb"];
-        progressionItems.MoveToBeginning(wallClimb);
-    }
-
-    /// <summary>
-    /// Finds all locations that are no longer reachable and removes them
-    /// </summary>
-    private void RemoveUnreachableLocations(LocationPool locations, GameInventory inventory)
-    {
-        locations.RemoveConditional(loc => !inventory.Evaluate(loc.Logic));
     }
 }
