a8e20f8c34e2e72312c44bc79ed3123220b8f23f
Branden Wood <44546325+BrandenEK@users.noreply.github.com>
2025-07-08 18:09:08 -0400

    core: require qol mod

--- BlasII.Randomizer/Properties/AssemblyInfo.cs
+++ BlasII.Randomizer/Properties/AssemblyInfo.cs
@@ -2,4 +2,4 @@ using BlasII.Randomizer;
 using MelonLoader;
 
 [assembly: MelonInfo(typeof(BlasII.Randomizer.Main), ModInfo.MOD_NAME, ModInfo.MOD_VERSION, ModInfo.MOD_AUTHOR)]
-[assembly: MelonOptionalDependencies("Modding API", "2.0.0", "Menu Framework", "0.3.0")]
+[assembly: MelonOptionalDependencies("Modding API", "2.0.0", "Menu Framework", "0.3.0", "Quality of Life", "2.0.1")]
