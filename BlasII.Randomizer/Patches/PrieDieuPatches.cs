842c808e9d0c1e3281c10db7d7fc6849ae4501a1
Branden Wood <44546325+BrandenEK@users.noreply.github.com>
2025-07-13 15:48:12 -0400

    game: add startroom option to pd menu (#214)
    * game: test startroom warp
    
    * core: upgrade qol version
    
    * game: implement startroom pd option

---
+++ BlasII.Randomizer/Patches/PrieDieuPatches.cs
@@ -0,0 +1,34 @@
+﻿using BlasII.ModdingAPI;
+using HarmonyLib;
+using Il2CppSystem;
+using Il2CppTGK.Game.Components.UI;
+
+namespace BlasII.Randomizer.Patches;
+
+/// <summary>
+/// Change teleport option to the starting room
+/// </summary>
+[HarmonyPatch(typeof(PrieuDieuMenuLogic), nameof(PrieuDieuMenuLogic.AddElement), typeof(string), typeof(string), typeof(Action))]
+class PrieuDieuMenuLogic_AddElement_Patch
+{
+    public static void Prefix(string itemName, ref string caption)
+    {
+        if (itemName == "TeleportToHUBUpgrade")
+            caption = Main.Randomizer.LocalizationHandler.Localize("dialog/travel");
+    }
+}
+
+/// <summary>
+/// Actually warp to the starting room
+/// </summary>
+[HarmonyPatch(typeof(PrieuDieuMenuLogic), nameof(PrieuDieuMenuLogic.ActionTeleportToHud))]
+class PrieuDieuMenuLogic_ActionTeleportToHud_Patch
+{
+    public static void Prefix()
+    {
+        ModLog.Info("Warping to starting room");
+
+        PrieuDieuMenuLogic.HUB_SCENE = 1291908248;
+        PrieuDieuMenuLogic.HUB_PRIE_DIEU = 931257951;
+    }
+}
--- BlasII.Randomizer/Patches/TestPatches.cs
+++ BlasII.Randomizer/Patches/TestPatches.cs
@@ -1,5 +1,9 @@
 ﻿using BlasII.ModdingAPI;
 using HarmonyLib;
+using Il2CppSystem;
+using Il2CppTGK.Game;
+using Il2CppTGK.Game.Components.UI;
+using Il2CppTGK.Game.Managers;
 
 namespace BlasII.Randomizer.Patches;
 
