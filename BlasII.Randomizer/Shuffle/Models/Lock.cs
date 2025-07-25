f53230e55937e854453e380f74593c4e3a2c1826
Branden Wood <44546325+BrandenEK@users.noreply.github.com>
2025-07-25 16:26:37 -0400

    shuffle: add the lock type

--- BlasII.Randomizer/Shuffle/Models/Lock.cs
+++ BlasII.Randomizer/Shuffle/Models/Lock.cs
@@ -5,7 +5,7 @@ namespace BlasII.Randomizer.Shuffle.Models;
 /// <summary>
 /// Represents an item that is locked to a location
 /// </summary>
-public class Lock(ItemLocation location, Item item)
+public class Lock(ItemLocation location, Item item, Lock.LockType type)
 {
     /// <summary>
     /// The location info
@@ -16,4 +16,25 @@ public class Lock(ItemLocation location, Item item)
     /// The item info
     /// </summary>
     public Item Item { get; } = item;
+
+    /// <summary>
+    /// The lock type
+    /// </summary>
+    public LockType Type { get; } = type;
+
+    /// <summary>
+    /// Determines how the lock will behave during shuffle
+    /// </summary>
+    [System.Flags]
+    public enum LockType
+    {
+        /// <summary> Has no effect on inventory or output </summary>
+        Nothing = 0x00,
+        /// <summary> Adds the item to the inventory when reached </summary>
+        AddToInventory = 0x01,
+        /// <summary> Adds the location to the output when reached </summary>
+        AddToOutput = 0x02,
+        /// <summary> Adds to inventory and output when reached </summary>
+        AddToBoth = AddToInventory | AddToOutput
+    }
 }
