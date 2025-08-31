f1a2f2e9856958c2ba1644015f9cdcd0a381b97b
Branden Wood <44546325+BrandenEK@users.noreply.github.com>
2025-08-31 14:54:54 -0400

    items: add basic display handler

---
+++ BlasII.Randomizer/ItemDisplay/DisplayInfo.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace BlasII.Randomizer.ItemDisplay;
+
+public class DisplayInfo(string message, string itemName, Sprite image)
+{
+    public string Message { get; } = message;
+
+    public string ItemName { get; } = itemName;
+
+    public Sprite Image { get; } = image;
+}
---
+++ BlasII.Randomizer/ItemDisplay/ItemDisplay.cs
@@ -0,0 +1,25 @@
+﻿using BlasII.Randomizer.Models;
+using System.Collections.Generic;
+
+namespace BlasII.Randomizer.ItemDisplay;
+
+public class ItemDisplay
+{
+    private readonly Queue<int> _itemQueue = [];
+
+    public void Show(Item item)
+    {
+
+    }
+
+    public void OnUpdate()
+    {
+        // Update popup
+    }
+
+    public void OnExitGame()
+    {
+        _itemQueue.Clear();
+        // Hide popup
+    }
+}
