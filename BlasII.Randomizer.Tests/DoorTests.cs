2932df375a9f1a7d4caf3c9f9bcba2fa3a6b5a9a
Branden Wood <44546325+BrandenEK@users.noreply.github.com>
2025-12-22 10:42:37 -0500

    tests: add tests for door info (#269)
    * tests: add datastorage
    
    * tests: test number of doors
    
    * tests: calculate directions before
    
    * tests: add more door tests
    
    * tests: start the pair test
    
    * tests: implement total door checker
    
    * tests: remove useless tests
    
    * data: fix door info

---
+++ BlasII.Randomizer.Tests/DoorTests.cs
@@ -0,0 +1,53 @@
+﻿using BlasII.Randomizer.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Text;
+
+namespace BlasII.Randomizer.Tests;
+
+[TestClass]
+public class DoorTests
+{
+    [TestMethod]
+    public void TestDoorMatches()
+    {
+        var sb = new StringBuilder();
+        bool invalid = false;
+
+        foreach (var entrance in DataStorage.Doors)
+        {
+            Door exit = DataStorage.GetDoor(entrance.Exit);
+
+            // The exit door does not lead back to the entrance
+            if (exit.Exit != entrance.Id)
+            {
+                sb.AppendLine($"{entrance.Id} != {exit.Id}'s exit");
+                invalid = true;
+                continue;
+            }
+
+            // The exit door has a different type
+            if (entrance.Type != exit.Type)
+            {
+                sb.AppendLine($"{entrance.Id} is {entrance.Type}, but {exit.Id} is {exit.Type}");
+                invalid = true;
+            }
+
+            // The exit door doesn't have the oppsite direction
+            if (entrance.Direction != GetOppositeDirection(exit))
+            {
+                sb.AppendLine($"{entrance.Id} is {entrance.Direction}, but {exit.Id} is {exit.Direction}");
+                invalid = true;
+            }
+        }
+
+        if (invalid)
+            throw new Exception(sb.ToString());
+    }
+
+    private static Door.DoorDirection GetOppositeDirection(Door door)
+    {
+        int direction = (int)door.Direction;
+        return (Door.DoorDirection)(direction % 2 == 0 ? direction + 1 : direction - 1);
+    }
+}
