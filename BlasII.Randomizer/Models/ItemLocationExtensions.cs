593158eb0e855982e10425e3cecb51c7a95685d7
Branden Wood <44546325+BrandenEK@users.noreply.github.com>
2025-12-22 18:50:11 -0500

    shuffle: remove shuffle check

--- BlasII.Randomizer/Models/ItemLocationExtensions.cs
+++ BlasII.Randomizer/Models/ItemLocationExtensions.cs
@@ -7,27 +7,21 @@ namespace BlasII.Randomizer.Models;
 public static class ItemLocationExtensions
 {
     /// <summary>
-    /// If this is a certain type of location, make sure the settings allow it to be shuffled
-    /// </summary>
-    public static bool ShouldBeShuffled(this ItemLocation location, RandomizerSettings settings)
-    {
-        if (string.IsNullOrEmpty(location.Flags))
-            return true;
-
-        //if (location.Flags.Contains('L') && !settings.ShuffleLongQuests)
-        //    return false;
-
-        //if (location.Flags.Contains('S') && !settings.ShuffleShops)
-        //    return false;
-
-        return true;
-    }
-
-    /// <summary>
     /// Checks if the location is marked with the specified flag
     /// </summary>
     public static bool HasFlag(this ItemLocation location, char flag)
     {
         return !string.IsNullOrEmpty(location.Flags) && location.Flags.Contains(flag);
+    }
+
+    /// <summary>
+    /// Checks if the location should be displayed as a valid location
+    /// </summary>
+    public static bool ContributesPercentage(this ItemLocation location, RandomizerSettings settings)
+    {
+        if (!settings.ShuffleCherubs && location.HasFlag('C'))
+            return false;
+
+        return true;
     }
 }
