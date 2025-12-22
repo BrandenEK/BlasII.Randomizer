8ef345c467ae2b2c59eb26592aa876e1a8686629
Branden Wood <44546325+BrandenEK@users.noreply.github.com>
2025-12-22 09:26:34 -0500

    tests: test number of doors

---
+++ BlasII.Randomizer.Tests/DoorTests.cs
@@ -0,0 +1,28 @@
+﻿using BlasII.Randomizer.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BlasII.Randomizer.Tests;
+
+[TestClass]
+public class DoorTests
+{
+    [TestMethod]
+    public void TestValidDirections()
+    {
+        var directions = new Dictionary<Door.DoorDirection, int>();
+
+        foreach (var door in DataStorage.Doors)
+        {
+            Door.DoorDirection d = door.Direction;
+
+            if (!directions.ContainsKey(d))
+                directions.Add(d, 1);
+            else
+                directions[d]++;
+        }
+
+        throw new System.Exception(string.Join(',', directions.Select(x => $"{x.Key}: {x.Value}")));
+    }
+}
