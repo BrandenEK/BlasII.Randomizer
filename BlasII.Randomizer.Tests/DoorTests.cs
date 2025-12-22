a45d09ccecb2d696b549042993f59dcbd05d4ced
Branden Wood <44546325+BrandenEK@users.noreply.github.com>
2025-12-22 09:54:38 -0500

    tests: add more door tests

--- BlasII.Randomizer.Tests/DoorTests.cs
+++ BlasII.Randomizer.Tests/DoorTests.cs
@@ -2,6 +2,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 
 namespace BlasII.Randomizer.Tests;
 
@@ -9,9 +10,10 @@ namespace BlasII.Randomizer.Tests;
 public class DoorTests
 {
     private static readonly Dictionary<Door.DoorDirection, int> _directions = new();
+    private static readonly Dictionary<Door.DoorType, int> _types = new();
 
     [ClassInitialize]
-    public static void SetupDirections(TestContext context)
+    public static void SetupClass(TestContext context)
     {
         foreach (var door in DataStorage.Doors)
         {
@@ -22,6 +24,41 @@ public class DoorTests
             else
                 _directions[d]++;
         }
+
+        foreach (var door in DataStorage.Doors)
+        {
+            Door.DoorType t = door.Type;
+
+            if (!_types.ContainsKey(t))
+                _types.Add(t, 1);
+            else
+                _types[t]++;
+        }
+    }
+
+    [TestMethod]
+    public void TestEvenNumberOfDoors()
+    {
+        Assert.IsTrue(DataStorage.Doors.Count() % 2 == 0);
+    }
+
+    [TestMethod]
+    public void TestEvenNumberOfEachType()
+    {
+        var sb = new StringBuilder();
+        bool invalid = false;
+
+        foreach (var kvp in _types)
+        {
+            if (kvp.Value % 2 == 0)
+                continue;
+
+            sb.AppendLine($"{kvp.Key} has {kvp.Value} doors");
+            invalid = true;
+        }
+
+        if (invalid)
+            throw new System.Exception(sb.ToString());
     }
 
     [TestMethod]
